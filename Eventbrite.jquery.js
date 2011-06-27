8b0b0cd6a4923596fec7e6c6fd4228d6e90d4e09
ryanjarvinen <ryan.jarvinen@gmail.com>
2011-06-27 14:58:23 -0700

    initial version of the Eventbrite javascript client library

---
+++ Eventbrite.jquery.js
@@ -0,0 +1,46 @@
+/*
+ * Eventbrite API client (jQuery required) - https://github.com/ryanjarvinen/Eventbrite.jquery.js
+ */
+
+//Constructor
+function Eventbrite() {
+  var args = Array.prototype.slice.call(arguments),
+    // the last argument is the callback
+    callback = args.pop(),
+    api_key = args[0],
+    user_key = args[1];
+    this.eventbrite_api_endpoint = "https://developer.eventbrite.com/json/";
+
+  // private accessor
+  authParams = function(params){
+    params.app_key = api_key;
+    params.user_key = user_key;
+    return params;
+  };
+
+  // make sure the function is called as a constructor
+  if (!(this instanceof Eventbrite)) {
+    return new Eventbrite(api_key, user_key, callback);
+  }
+  
+  // call callback
+  callback(this);
+}
+
+Eventbrite.prototype = {
+  request: function( method, params, cb)
+  {
+    $.ajax({
+      url: this.eventbrite_api_endpoint + method,
+      data: authParams(params),
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(resp){
+        cb(resp.contents);
+      },
+      failure: function(err){
+        console.log("Error connecting to Eventbrite API");
+      }
+    });
+  }
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# An Eventbrite API clinet library. (requires jQuery)
+--------------------------------------
+written by @ryanjarvinen
+see http://creativecommons.org/licenses/by/3.0/ for license info
+see http://developer.eventbrite.com/terms/ for Eventbrite API terms and usage limitations
+see http://developer.eventbrite.com/news/branding/ for branding guidelines
+
+
+
